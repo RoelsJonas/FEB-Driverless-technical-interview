5c38288ec826fb3b9844fae5cd1cd66499d85257
Jonas Roels <roels.jonas@protonmail.com>
2024-04-06 22:18:36 +0200

    Cleaned up main + fix output

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class Main {
     public static final String inputFileName = "recruitment.txt";
     public static final String outputFileName = "output.txt";
+    public static final boolean DEBUG = false;
     public static boolean[][] track;
     public static Node[][] nodes;
     public static ArrayList<Node> path;
@@ -12,30 +13,88 @@ public class Main {
     public static ArrayList<Node> bestPath;
     public static void main(String[] args) throws IOException {
         long startMillis = System.currentTimeMillis();
+        ArrayList<ArrayList<Character>> map = readInput();
+        createNodes(map);
+        initializeGraph(map);
 
-        // read the input file
-        ArrayList<ArrayList<Character>> map = new ArrayList<>();
-        File file = new File(inputFileName);
-        Scanner scanner = new Scanner(file);
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            ArrayList<Character> row = new ArrayList<>();
-            for (int i = 0; i < line.length(); i++) {
-                row.add(line.charAt(i));
-            }
-            map.add(row);
-        }
-
-        // create nodes
-        nodes = new Node[map.size()][map.get(0).size()];
-        for (int i = 0; i < map.size(); i++) {
-            for (int j = 0; j < map.get(0).size(); j++) {
-                if(map.get(i).get(j).charValue() == '0') nodes[i][j] = new Node(i, j);
-                else nodes[i][j] = null;
+        // print the map
+        if(DEBUG) {
+            for (int i = 0; i < map.size(); i++) {
+                for (int j = 0; j < map.get(0).size(); j++) {
+                    if (nodes[i][j] == null) System.out.print("X");
+                    else System.out.print("0");
+                }
+                System.out.println();
             }
         }
 
-        // add neighbors and set initial bounds
+        // run the branch and bound algorithm
+        path = new ArrayList<>();
+        score = 0;
+        bestScore = Integer.MAX_VALUE;
+        // loop over all possible starting positions
+        for(int i = 0; i < nodes[0].length; i++) {
+            if(nodes[0][i] == null) continue;
+            path.add(nodes[0][i]);
+            branchAndBound(nodes[0][i]);
+            path.remove(0);
+        }
+
+        generateOutput(map);
+        writeOutput(map);
+
+        // print the output
+        if(DEBUG) {
+            for(int i = 0; i < map.size(); i++) {
+                for(int j = 0; j < map.get(0).size(); j++) {
+                    System.out.print(map.get(i).get(j));
+                }
+                System.out.println();
+            }
+        }
+
+        // print the processing time
+        System.out.println("Processing time: " + (System.currentTimeMillis() - startMillis) + "ms");
+    }
+
+    // write the output to a file
+    private static void writeOutput(ArrayList<ArrayList<Character>> map) throws IOException {
+        FileWriter writer = new FileWriter(outputFileName);
+        for(int i = 0; i < map.size(); i++) {
+            for(int j = 0; j < map.get(0).size(); j++) {
+                writer.write(map.get(i).get(j));
+            }
+            writer.write("\n");
+        }
+        writer.write("Total cost: " + bestScore + "\n");
+        writer.close();
+    }
+
+    // generate the output path
+    private static void generateOutput(ArrayList<ArrayList<Character>> map) {
+        for(int i = 0; i < bestPath.size() - 1; i++) {
+            Node current = bestPath.get(i);
+            Node next = bestPath.get(i+1);
+
+            if(current.x == next.x) {
+                if(current.y < next.y) map.get(current.x).set(current.y, '>');
+                else map.get(current.x).set(current.y, '<');
+            } else if(current.y == next.y) {
+                if(current.x < next.x) map.get(current.x).set(current.y, 'v');
+                else map.get(current.x).set(current.y, '^');
+            } else {
+                if(current.x < next.x && current.y < next.y) map.get(current.x).set(current.y, '\\');
+                else if(current.x < next.x && current.y > next.y) map.get(current.x).set(current.y, '/');
+                else if(current.x > next.x && current.y < next.y) map.get(current.x).set(current.y, '/');
+                else map.get(current.x).set(current.y, '\\');
+            }
+        }
+        Node last = bestPath.get(bestPath.size()-1);
+        map.get(last.x).set(last.y, 'v');
+    }
+
+    // initialize the graph with neighbors and bounds
+    private static void initializeGraph(ArrayList<ArrayList<Character>> map) {
         for (int i = 0; i < map.size(); i++) {
             for (int j = 0; j < map.get(0).size(); j++) {
                 if(nodes[i][j] == null) continue;
@@ -56,70 +115,36 @@ public class Main {
                 nodes[i][j].upperbound = Integer.MAX_VALUE/2;
             }
         }
-
-        // print the map
-        for(int i = 0; i < map.size(); i++) {
-            for(int j = 0; j < map.get(0).size(); j++) {
-                if(nodes[i][j] == null) System.out.print("X");
-                else System.out.print("0");
-            }
-            System.out.println();
-        }
-
-        // run the branch and bound algorithm
-        path = new ArrayList<>();
-        score = 0;
-        bestScore = Integer.MAX_VALUE;
-        for(int i = 0; i < nodes[0].length; i++) {
-            if(nodes[0][i] == null) continue;
-            path.add(nodes[0][i]);
-            branchAndBound(nodes[0][i]);
-            path.remove(0);
-        }
-
-        // generate output
-        for(int i = 0; i < bestPath.size() - 1; i++) {
-            Node current = bestPath.get(i);
-            Node next = bestPath.get(i+1);
-
-            if(current.x == next.x) {
-                if(current.y < next.y) map.get(current.x).set(current.y, '>');
-                else map.get(current.x).set(current.y, '<');
-            } else if(current.y == next.y) {
-                if(current.x < next.x) map.get(current.x).set(current.y, 'v');
-                else map.get(current.x).set(current.y, '^');
-            } else {
-                if(current.x < next.x && current.y < next.y) map.get(current.x).set(current.y, '\\');
-                else if(current.x < next.x && current.y > next.y) map.get(current.x).set(current.y, '/');
-                else if(current.x > next.x && current.y < next.y) map.get(current.x).set(current.y, '/');
-                else map.get(current.x).set(current.y, '\\');
-            }
-        }
-        Node last = bestPath.get(bestPath.size()-1);
-        map.get(last.x).set(last.y, 'v');
-
-        // print the output
-        for(int i = 0; i < map.size(); i++) {
-            for(int j = 0; j < map.get(0).size(); j++) {
-                System.out.print(map.get(i).get(j));
-            }
-            System.out.println();
-        }
-
-        // write the output to a file
-        FileWriter writer = new FileWriter(outputFileName);
-        for(int i = 0; i < map.size(); i++) {
-            for(int j = 0; j < map.get(0).size(); j++) {
-                writer.write(map.get(i).get(j));
-            }
-            writer.write("\n");
-        }
-        writer.close();
-
-        // print the processing time
-        System.out.println("Processing time: " + (System.currentTimeMillis() - startMillis) + "ms");
     }
 
+    // create the nodes from the map
+    private static void createNodes(ArrayList<ArrayList<Character>> map) {
+        nodes = new Node[map.size()][map.get(0).size()];
+        for (int i = 0; i < map.size(); i++) {
+            for (int j = 0; j < map.get(0).size(); j++) {
+                if(map.get(i).get(j).charValue() == '0') nodes[i][j] = new Node(i, j);
+                else nodes[i][j] = null;
+            }
+        }
+    }
+
+    // read the input from the file
+    private static ArrayList<ArrayList<Character>> readInput() throws FileNotFoundException {
+        ArrayList<ArrayList<Character>> map = new ArrayList<>();
+        File file = new File(inputFileName);
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            ArrayList<Character> row = new ArrayList<>();
+            for (int i = 0; i < line.length(); i++) {
+                row.add(line.charAt(i));
+            }
+            map.add(row);
+        }
+        return map;
+    }
+
+    // recursive branch and bound algorithm
     public static void branchAndBound(Node currentNode) {
         // check if destination is reached
         if(currentNode.x == nodes.length-1) {
